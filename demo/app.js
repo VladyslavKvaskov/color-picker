cb5ad74007d6cf6cb861cc6f0556159a46621a72
Vlad Kvaskov <vladkvaskov@yahoo.com>
2020-10-27 17:55:20 +0200

    Update app.js

--- demo/app.js
+++ demo/app.js
@@ -1,10 +1,15 @@
-if (typeof color_picker !== 'undefined') {
-
+for(const color_picker of document.querySelectorAll('color-picker')){
   color_picker.addEventListener('select', () => {
     console.log(color_picker.value);
   });
 
   color_picker.addEventListener('selecting', () => {
     console.log(color_picker.futureValue);
+  });
+}
+
+for(const rangeSlider of document.querySelectorAll('range-slider')){
+  rangeSlider.addEventListener('input', ()=>{
+    console.log(rangeSlider.value.x);
   });
 }
